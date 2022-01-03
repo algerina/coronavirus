1b0d5636c0c68b951256acc458527941b9ace039
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-01-03 22:11:49 +0100

    Remove boilerplate

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+        <nav>
+        </nav>
+        </div>
+    );
+}
+
+export default Navbar;
