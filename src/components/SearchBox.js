6bbd4ee6c5991766f51787cb2de64499b632ba66
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-01-13 23:26:34 +0100

    Fix API issue

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { searchCountry } from '../redux/reducers/dataReducer';
 
-const SearchBox = ({ searchCountry }) => (
+const SearchBox = ({ searchfield, SearchChange }) => (
   <div className="pa2">
     <input
       className="pa3 ba b--pink bg-lightest-pink"
       type="search"
       placeholder="Choose a country"
-      onChange={SearchCountry}
+      onChange={SearchChange}
     />
   </div>
 );
