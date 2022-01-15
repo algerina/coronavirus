cf559579d56e09e48521070f5bafc53eafe763ef
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-01-15 18:52:15 +0100

    Fix linter issues

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import {
-  render, fireEvent, screen, cleanup,
-} from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import App from '../App';
